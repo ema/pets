7d73d22a5f2d4fd7bd1c59d919a7de51c8b7b9b5
Emanuele Rocca <ema@linux.it>
2022-10-21 10:18:33 +0200

    Parser WIP

--- parser.go
+++ parser.go
@@ -10,36 +10,82 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
-func readModelines(path string) error {
-	maxLines := 10
+const MAXLINES int = 10
+
+func readModelines(path string) ([]string, error) {
+	modelines := []string{}
 
 	file, err := os.Open(path)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	scannedLines := 0
 	for scanner.Scan() {
-		if maxLines == 0 {
-			return nil
+		if scannedLines == MAXLINES {
+			return modelines, nil
 		}
 
 		line := scanner.Text()
 
 		if strings.Contains(line, "pets:") {
-			fmt.Println(line)
+			modelines = append(modelines, line)
 		}
 
-		maxLines -= 1
+		scannedLines += 1
 	}
+	return modelines, nil
+}
+
+func parseModeline(line string, pf *PetsFile) error {
+	// We just ignore and throw away anything before the 'pets:' modeline
+	// identifier
+	re, err := regexp.Compile("pets:(.*)")
+	if err != nil {
+		return err
+	}
+
+	// Hope for the best but prepare for the worst. Here's the error object
+	// we're gonna return if things go wrong.
+	lineError := fmt.Errorf("ERR: invalid pets modeline: %v", line)
+
+	matches := re.FindStringSubmatch(line)
+
+	if len(matches) < 2 {
+		return lineError
+	}
+
+	fmt.Println("---")
+
+	components := strings.Split(matches[1], " ")
+	for i := 0; i < len(components); i++ {
+		elem := components[i]
+
+		if len(elem) == 0 {
+			continue
+		}
+
+		directive, _, _ := strings.Cut(elem, "=")
+
+		if directive == "destfile" {
+			fmt.Printf("\tUHUH found destfile -> %v\n", components)
+		} else if directive == "package" {
+			fmt.Printf("\tUHUH found package -> %v\n", components)
+		}
+	}
+
 	return nil
 }
 
-func handler(path string, info os.FileInfo, err error) error {
+// This function is called once for each file in the Pets configuration
+// directory
+func petsFileHandler(path string, info os.FileInfo, err error) error {
 	if err != nil {
 		return err
 	}
@@ -49,15 +95,38 @@ func handler(path string, info os.FileInfo, err error) error {
 		return nil
 	}
 
-	fmt.Println("***", path, info.Size(), "***")
-	readModelines(path)
+	modelines, err := readModelines(path)
+	if err != nil {
+		return err
+	}
 
-	// TODO: figure out if the file at path contains any pets modelines
+	if len(modelines) == 0 {
+		// Not a Pets file. We don't take it personal though
+		return nil
+	}
 
-	return nil
+	fmt.Printf("-> %d pets modelines found in %s\n", len(modelines), path)
+
+	// Instantiate a PetsFile representation. The only thing we know so far
+	// is the source path. Every long journey begins with a single step!
+	pf := &PetsFile{
+		Source: path,
+	}
+
+	for _, line := range modelines {
+		err := parseModeline(line, pf)
+		if err != nil {
+			// TODO: Possibly a syntax error, skip the whole file
+			// (this very "path")
+			return err
+		}
+	}
+
+	fmt.Println(pf)
+	return err
 }
 
 func walkDir(directory string) error {
-	err := filepath.Walk(directory, handler)
+	err := filepath.Walk(directory, petsFileHandler)
 	return err
 }
